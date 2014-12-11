7320ecf1e5efd3bd85f5bd9c74ffe1c4417be00d
DanielaOyarce <daidany29@gmail.com>
2014-12-11 10:02:14 -0300

    Actualizacion 11_12_2014

--- mapi/views.py
+++ mapi/views.py
@@ -14,11 +14,16 @@ from aircraft.models import Aircraft, Fleet, Operator
 def DateExcelToPython(workbook, sheet, rx, cell):
     try:
         xldate = sheet.cell_value(rowx=rx, colx=cell)
-        tuple_date = xlrd.xldate_as_tuple(xldate, workbook.datemode)
-        pydatetime = dt.datetime(*tuple_date)
-        return pydatetime
+        if xldate:
+            tuple_date = xlrd.xldate_as_tuple(xldate, workbook.datemode)
+            pydatetime = dt.datetime(*tuple_date)
+            pydatetime = pydatetime.strftime("%Y-%m-%d %H:%M:%S")
+            return pydatetime        
+        else:
+            pydatetime = None
+            return pydatetime
+
     except Exception, e:
-        print e
 
 
 def form_mapi(request):
@@ -26,45 +31,22 @@ def form_mapi(request):
         data = request.FILES['archivo']
         workbook = xlrd.open_workbook(file_contents=data.read())
         sheet = workbook.sheet_by_index(0)
-
-        print sheet.name, sheet.nrows, sheet.ncols     
+ 
         
         if sheet.ncols == 20:
             for rx in range(1, sheet.nrows):
                 rows = sheet.row_values(rx)
-                print rows[1]
-                
+
                 if sheet.cell_type(rx, 1) != 0:
                     fleet, created = Fleet.objects.get_or_create(name=rows[0])
                     aircraft, created = Aircraft.objects.get_or_create(name=rows[1], 
                         defaults={'fleet': fleet})
 
-                    if rows[6]:
-                        dtlmfl = DateExcelToPython(workbook, sheet, rx, 6)
-                        dd1 = dtlmfl.strftime("%Y-%m-%d %H:%M:%S")
-                        print dd1
-                    else:
-                        dd1 = '2000-01-01 00:00:00'
-                        print 'Vacio col 6'
+                    dtlmfl = DateExcelToPython(workbook, sheet, rx, 6)
+                    duedate = DateExcelToPython(workbook, sheet, rx, 13)
+                    found_on_date = DateExcelToPython(workbook, sheet, rx, 19)
 
-                    if rows[13]:
-                        duedate = DateExcelToPython(workbook, sheet, rx, 13)
-                        dd2 = duedate.strftime("%Y-%m-%d %H:%M:%S")
-                        print dd2
-                    else:
-                        dd2 = '2000-01-01 00:00:00'
-                        print 'Vacio col 13'
-
-                    if rows[19]:
-                        found_on_date = DateExcelToPython(workbook, sheet, rx, 19)
-                        dd3 = found_on_date.strftime("%Y-%m-%d %H:%M:%S")
-                        print dd3
-                    else:
-                        #print 'Error'
-                        dd2 = '2000-01-01 00:00:00'
-                        print 'Vacio col 19'
-
-
+  
 
                     # Save Pireps & Mareps
                     mapi, created = Mapi.objects.update_or_create(nri=rows[10], 
@@ -74,18 +56,18 @@ def form_mapi(request):
                         'subata': rows[3],
                         'week': rows[4],
                         'nmfl': rows[5],
-                        'dtlmfl': dd1,
+                        'dtlmfl': dtlmfl,
                         'flight_number': rows[7],
                         'sta': rows[8],
                         'reference_term': rows[9],
                         'dmi': rows[11],
                         'cat': rows[12],
-                        'duedate': dd2,
+                        'duedate': duedate,
                         'discrepancies': rows[14],
                         'action_correct': rows[15],
                         'part_number': rows[16],
                         'position': rows[17],
                         'status': rows[18],
-                        'found_on_date': dd3
+                        'found_on_date': found_on_date
                       })
     return render(request,'mapi/form_mapi.html')
